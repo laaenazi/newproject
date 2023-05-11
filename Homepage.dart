595b1482998498021a8231a4700cab90e2aa4311
laaenazi <133174442+laaenazi@users.noreply.github.com>
2023-05-11 06:34:43 +0300

    Create Homepage.dart

---
+++ Homepage.dart
@@ -0,0 +1,99 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'homepge_model.dart';
+export 'homepge_model.dart';
+
+class HomepgeWidget extends StatefulWidget {
+  const HomepgeWidget({Key? key}) : super(key: key);
+
+  @override
+  _HomepgeWidgetState createState() => _HomepgeWidgetState();
+}
+
+class _HomepgeWidgetState extends State<HomepgeWidget>
+    with TickerProviderStateMixin {
+  late HomepgeModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => HomepgeModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      context.pushNamed('login');
+    });
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Color(0xFF1E2429),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF4B39EF), Color(0xFFEE8B60)],
+            stops: [0, 1],
+            begin: AlignmentDirectional(1, -1),
+            end: AlignmentDirectional(-1, 1),
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/ff_logo_white.png',
+              width: 140,
+              height: 140,
+              fit: BoxFit.fitHeight,
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+              child: Text(
+                'مرحبا',
+                style: FlutterFlowTheme.of(context).displaySmall.override(
+                      fontFamily: 'Lexend Deca',
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 120),
+              child: Text(
+                'كيف الحال',
+                style: FlutterFlowTheme.of(context).headlineSmall.override(
+                      fontFamily: 'Lexend Deca',
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
